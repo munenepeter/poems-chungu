d4ec3e37e51d0b03cee0d14838320335c755aea4
munenepeter <peternjega7@gmail.com>
2023-08-09 19:26:10 +0300

    a lot of changes

--- Views/index.view.php
+++ Views/index.view.php
@@ -1,28 +1,47 @@
 <?php include_once 'base.view.php'; ?>
 <?php include_once 'sections/nav.view.php'; ?>
 
-<div class="bg-slate-500 h-64 pt-8  mt-16 ">
+<section class="bg-slate-600 md:h-64 lg:72 pt-8  md:mt-16 mt-8 ">
+    <div class="px-4 py-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-20 lg:py-2">
+        <div class="flex flex-col justify-between lg:flex-row px-5">
+            <div class="mb-8 lg:max-w-lg lg:pr-5 lg:mb-0">
+                <h1 class="text-white font-extrabold md:text-6xl text-4xl">New poetry, every week</h1>
+            </div>
+            <div class="pb-5 lg:w-2/5">
+                <p class='pb-8 text-gray-400'>Sign up tp get new poetry playlist in your inbox every week.You'll receive
+                    set
+                    of poems around a different theme, with a focus on contemporary poets. I'ts free, and you can
+                    unsubsribe anytime. Read more about us</p>
 
-    <div class="flex items-start justify-center container px-4 mx-auto">
-        <div class="px-2 sm:w-1/2 md:w-1/4">
-            <h1 class="text-white font-extrabold text-5xl">New Poems every week</h1>
-        </div>
-
-        <div class="px-2 sm:w-1/2 md:w-3/4">
-
-            <p class='text-gray-400'>Sign up tp get new poetry playlist in your inbox every week.</p>
-
-            <div class='flex xsm:flex-row md:p-0'>
-                <input
-                    class='px-4 py-3 text-lg transition-all duration-300 rounded-l-lg focus:outline-none focus:right-1 '
-                    placeholder='Enter your email' type="text" />
-                <button class='-ml-1 px-4 py-3 font-semibold bg-amber-500 rounded-r-lg'>Subscribe</button>
+                <div class='flex sm:flex-row md:p-0'>
+                    <input class='px-8 py-3 text-lg transition-all duration-300 focus:outline-none focus:right-1 '
+                        placeholder='Enter your email' type="text" />
+                    <button class='ml-1 px-6 py-3 font-semibold bg-amber-500'>Subscribe</button>
+                </div>
             </div>
         </div>
-
     </div>
-</div>
+</section>
 
+
+<div class="px-4 py-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-20 lg:py-2">
+    <div class="grid grid-cols-6 gap-4 mx-auto">
+        <h1>Latest</h1>
+        <div class="-ml-24 font-medium col-span-3">
+            
+            <h2>Lorem ipsum dolor sit.</h2>
+
+            <small>Lorem, ipsum.</small>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium iusto a reprehenderit, vero quibusdam totam voluptate quod! Nisi pariatur non sed consequuntur eius cumque soluta, reiciendis quos exercitationem? Voluptatibus, debitis.</p>
+        </div>
+
+        <div class="flex flex-col col-span-2">
+            1
+        </div>
+    </div>
+
+</div>
 
 
 <?php include_once 'sections/footer.view.php'; ?>
--- Views/sections/nav.view.php
+++ Views/sections/nav.view.php
@@ -1,33 +1,27 @@
-<nav class="bg-slate-500 fixed w-full top-0 -mb-2">
+<nav class="bg-slate-600 fixed w-full top-0 -mb-2">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <a href="/" class="flex items-center text-amber-500">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-        stroke="currentColor" class="w-8 h-8 mr-3">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+        class="w-8 h-8 mr-3 hidden md:block">
         <path stroke-linecap="round" stroke-linejoin="round"
           d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
       </svg>
-      <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Chungu Poems</span>
+      <span class="self-center md:text-2xl text-lg font-semibold whitespace-nowrap dark:text-white">Chungu Poems</span>
     </a>
-    <div class="flex md:order-2">
-      <button type="button" class="text-white bg-amber-700 hover:bg-amber-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-amber-600 dark:hover:bg-amber-700 dark:focus:ring-amber-800">Subscribe</button>
-      <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
-        <span class="sr-only">Open main menu</span>
-        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
-        </svg>
-      </button>
-    </div>
-    <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-      <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0  bg-transparent">
-       
+
+    <div class="" id="navbar-stick9pipiy">
+      <ul class="flex md:space-x-8 space-x-2 bg-transparent font-medium uppercase">
         <li>
-          <a href="#" class="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+          <a href="#"
+            class="block py-2 pl-1 md:pl-3 pr-2 md:pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-amber-500 md:p-0 md:dark:hover:text-amber-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
         </li>
         <li>
-          <a href="#" class="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Poems</a>
+          <a href="#"
+            class="block py-2 pl-1 md:pl-3 pr-2 md:pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-amber-500 md:p-0 md:dark:hover:text-amber-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Poems</a>
         </li>
         <li>
-          <a href="#" class="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Reviews</a>
+          <a href="#"
+            class="block py-2 pl-1 md:pl-3 pr-2 md:pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-amber-500 md:p-0 md:dark:hover:text-amber-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Reviews</a>
         </li>
       </ul>
     </div>
