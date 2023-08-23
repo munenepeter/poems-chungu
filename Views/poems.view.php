77f575b239ca50b8fe7b79fa2de01bdfe3cf98a5
munenepeter <peternjega7@gmail.com>
2023-08-23 19:32:42 +0300

    status

--- Views/poems.view.php
+++ Views/poems.view.php
@@ -12,7 +12,7 @@ include_once 'sections/nav.view.php'; ?>
                                           <svg class="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                                  <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
                                           </svg>
-                                          Members only
+                                          <?= ($poem->status) ? "Published" : "Unpublished"; ?>
                                    </p>
                                    <div class="text-gray-900 font-bold text-xl mb-2"><?= $poem->title; ?></div>
                                    <p class="text-gray-700 text-base"><?= wp_strip_all_tags(htmlspecialchars_decode($poem->body)) ?></p>
