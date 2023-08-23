87870180543bbce4479d2115e984dca2b467c9a3
munenepeter <peternjega7@gmail.com>
2023-08-23 19:30:05 +0300

    all poems

--- Views/poems.view.php
+++ Views/poems.view.php
@@ -1,2 +1,30 @@
-<?php include_once 'base.view.php'; include_once 'sections/nav.view.php';?><div class="grid place-items-center h-screen">
-       TO add poems</div>
+<?php include_once 'base.view.php';
+include_once 'sections/nav.view.php'; ?>
+<div class="w-full lg:max-w-full pt-8 md:mt-32 mt-8 px-4 py-4 lg:py-8">
+
+
+       <article class="grid md:grid-cols-4 grid-cols-1 w-full mt-8 gap-2">
+
+              <?php foreach ($poems as $poem) : ?>
+                     <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                            <div class="mb-4">
+                                   <p class="text-sm text-gray-600 flex items-center">
+                                          <svg class="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                                 <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
+                                          </svg>
+                                          Members only
+                                   </p>
+                                   <div class="text-gray-900 font-bold text-xl mb-2"><?= $poem->title; ?></div>
+                                   <p class="text-gray-700 text-base"><?= wp_strip_all_tags(htmlspecialchars_decode($poem->body)) ?></p>
+                            </div>
+
+                            <div class="text-sm">
+                                   <p class="text-gray-900 leading-none"><?= $poem->author()->name; ?></p>
+                                   <p class="text-gray-600"><?= $poem->updated_at; ?></p>
+                            </div>
+
+                     </div>
+              <?php endforeach; ?>
+       </article>
+
+</div>
