0befa0a2696731ed62fa2730d68e0ae8c897176e
munenepeter <peternjega7@gmail.com>
2023-08-24 16:48:45 +0300

    add icons for actions

--- Views/manage-poems.view.php
+++ Views/manage-poems.view.php
@@ -107,7 +107,47 @@ include_once 'sections/nav.view.php';
                                           </section>
                                    </div>
 
-                                   <table class="min-w-full divide-y divide-asparagus-400">
+                                   <article class="md:hidden min-w-full grid md:grid-cols-4 w-full mt-8 gap-2 bg-asparagus-50 ">
+                                          <?php if (!empty($poems)) : ?>
+                                                 <?php foreach ($poems as $poem) : ?>
+                                                        <div class="bg-clip-border border-4 border-white border-l-asparagus-500 bg-white p-4 flex flex-col justify-between leading-normal">
+                                                               <div class="mb-4">
+                                                                      <div class="flex justify-between">
+                                                                             <p class="text-sm text-gray-600 flex items-center">
+                                                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="fill-current text-gray-500 w-3 h-3 mr-2">
+                                                                                           <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
+                                                                                    </svg>
+                                                                                    <?= ($poem->status) ? "Published" : "Unpublished"; ?>
+                                                                             </p>
+                                                                             <p class="text-sm text-gray-600 flex items-center">
+                                                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-green-500 w-5 h-5">
+                                                                                           <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                                                                                    </svg>
+                                                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-purple-500 w-5 h-5">
+                                                                                           <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
+                                                                                    </svg>
+                                                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-red-500 w-5 h-5">
+                                                                                           <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                                                                    </svg>
+                                                                             </p>
+                                                                      </div>
+                                                                      <div class="text-gray-900 font-bold text-xl mb-2"><?= $poem->title; ?></div>
+                                                                      <p class="text-gray-700 text-base"><?= truncate(format_poem($poem->body), 155) ?></p>
+                                                               </div>
+
+
+                                                               <div class="text-sm">
+                                                                      <p class="text-gray-900 leading-none"><?= $poem->author()->name; ?></p>
+                                                                      <p class="text-gray-600"><?= $poem->category()->name; ?></p>
+                                                               </div>
+
+
+                                                        </div>
+                                                 <?php endforeach; ?>
+                                          <?php endif; ?>
+                                   </article>
+
+                                   <table class="md:block hidden min-w-full divide-y divide-asparagus-400">
                                           <thead class="bg-asparagus-100">
                                                  <tr>
                                                         <th scope="col" class="px-4 py-3 text-left text-xs font-medium text-asparagus-500 uppercase tracking-wider">
@@ -135,17 +175,20 @@ include_once 'sections/nav.view.php';
                                                                              <p> <?= $poem->title ?></p>
                                                                       </td>
                                                                       <td class="px-6 py-4 whitespace-nowrap">
-                                                                             <p><?= $poem->author ?></p>
+                                                                             <p><?= $poem->author()->name ?></p>
 
                                                                       </td>
                                                                       <td class="px-6 py-4 ">
                                                                              <p class="text-sm text-asparagus-900">
                                                                                     <?= format_poem($poem->body) ?>
                                                                              </p>
+
                                                                       </td>
 
                                                                       <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                                                             <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                                                                             <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a> |
+                                                                             <a href="#" class="text-purple-600 hover:text-purple-900">Publish</a>|
+                                                                             <a href="#" class="text-red-600 hover:text-red-900">Delete</a>
                                                                       </td>
                                                                </tr>
                                                         <?php endforeach; ?>
--- Views/poems.view.php
+++ Views/poems.view.php
@@ -4,27 +4,28 @@ include_once 'sections/nav.view.php'; ?>
 
 
        <article class="grid md:grid-cols-4 w-full mt-8 gap-2 bg-asparagus-50 ">
+              <?php if (!empty($poems)) : ?>
+                     <?php foreach ($poems as $poem) : ?>
+                            <div class="bg-clip-border border-4 border-white border-l-asparagus-500 bg-white p-4 flex flex-col justify-between leading-normal">
+                                   <div class="mb-4">
+                                          <p class="text-sm text-gray-600 flex items-center">
+                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="fill-current text-gray-500 w-3 h-3 mr-2">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
+                                                 </svg>
+                                                 <?= ($poem->status) ? "Published" : "Unpublished"; ?>
+                                          </p>
+                                          <div class="text-gray-900 font-bold text-xl mb-2"><?= $poem->title; ?></div>
+                                          <p class="text-gray-700 text-base"><?= truncate(format_poem($poem->body), 155) ?></p>
+                                   </div>
 
-              <?php foreach ($poems as $poem) : ?>
-                     <div class="bg-clip-border border-4 border-white border-l-asparagus-500 bg-white p-4 flex flex-col justify-between leading-normal">
-                            <div class="mb-4">
-                                   <p class="text-sm text-gray-600 flex items-center">
-                                          <svg class="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                                 <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
-                                          </svg>
-                                          <?= ($poem->status) ? "Published" : "Unpublished"; ?>
-                                   </p>
-                                   <div class="text-gray-900 font-bold text-xl mb-2"><?= $poem->title; ?></div>
-                                   <p class="text-gray-700 text-base"><?= truncate(format_poem($poem->body), 155) ?></p>
+                                   <div class="text-sm">
+                                          <p class="text-gray-900 leading-none"><?= $poem->author()->name; ?></p>
+                                          <p class="text-gray-600"><?= $poem->updated_at; ?></p>
+                                   </div>
+
                             </div>
-
-                            <div class="text-sm">
-                                   <p class="text-gray-900 leading-none"><?= $poem->author()->name; ?></p>
-                                   <p class="text-gray-600"><?= $poem->updated_at; ?></p>
-                            </div>
-
-                     </div>
-              <?php endforeach; ?>
+                     <?php endforeach; ?>
+              <?php endif; ?>
        </article>
 
 </div>
