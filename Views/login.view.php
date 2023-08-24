b777ba78f8d03c46b0f06c65ef743e141ebf911a
munenepeter <peternjega7@gmail.com>
2023-08-24 16:15:32 +0300

    end html

--- Views/login.view.php
+++ Views/login.view.php
@@ -1,9 +1,7 @@
 <?php include_once 'base.view.php'; ?>
 
 <div class="grid place-items-center h-screen md:p-0 p-2 bg-white">
-
        <div class="w-full max-w-sm p-4 bg-asparagus-50 border rounded-lg sm:p-6 md:p-8 " style="background-color: rgb(244, 247, 238); border-color: rgb(152, 179, 104);">
-
               <form class="space-y-6" action="/auth/login" method="post">
                      <h5 class="text-xl font-bold text-asparagus-900 text-center">Welcome back to Chungu Poems</h5>
                      <div>
@@ -18,7 +16,7 @@
                             <input type="password" name="password" id="password" placeholder="••••••••" class="bg-asparagus-50 border border-asparagus-300 text-asparagus-800 rounded-lg
                                    focus:ring-asparagus-500 focus:border-asparagus-500 block w-full p-2.5 " required>
                      </div>
-                     <p class="<?= $errors ? '': 'hidden'?> text-red-500"><?= $errors ?></p>
+                     <p class="<?= $errors ? '' : 'hidden' ?> text-red-500"><?= $errors ?></p>
                      <button type="submit" class="w-full text-white bg-asparagus-500 hover:bg-asparagus-400 focus:ring-4 focus:outline-none
                      focus:ring-asparagus-300 font-medium rounded-lg  px-5 py-2.5 text-center
                      dark:hover:bg-asparagus-800 dark:focus:ring-asparagus-800">Login
@@ -28,8 +26,8 @@
                                    account</a>
                      </div>
               </form>
-
        </div>
-
-
 </div>
+</body>
+
+</html>
