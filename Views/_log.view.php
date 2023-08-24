1eb783b00ee3c2c9256c93ee3612faee45cc010f
munenepeter <peternjega7@gmail.com>
2023-08-24 15:45:53 +0300

    fix bug with logger for <br>

--- Views/_log.view.php
+++ Views/_log.view.php
@@ -10,20 +10,16 @@ $all = implode(",", $logs);
     <div class="w-full p-4">
         <div class="w-full flex items-center py-2">
             <div class="hidden md:flex items-center">
-                <button type="button"
-                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Debug
+                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Debug
                     <?= substr_count($all, 'Debug') ?>
                 </button>
-                <button type="button"
-                    class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Info
+                <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Info
                     <?= substr_count($all, 'Info') ?>
                 </button>
-                <button type="button"
-                    class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Errors
+                <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Errors
                     <?= substr_count($all, 'Error') ?>
                 </button>
-                <button type="button"
-                    class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2 mr-2 mb-2 dark:focus:ring-yellow-900">Warnings
+                <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2 mr-2 mb-2 dark:focus:ring-yellow-900">Warnings
                     <?= substr_count($all, 'Warning') ?>
                 </button>
             </div>
@@ -32,22 +28,16 @@ $all = implode(",", $logs);
                     <label for="simple-search" class="sr-only">Search</label>
                     <div class="relative w-full">
                         <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor"
-                                viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd"
-                                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                                    clip-rule="evenodd"></path>
+                            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path>
                             </svg>
                         </div>
-                        <input type="text" id="simple-search"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            placeholder="Search ..." required="">
+                        <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search ..." required="">
                     </div>
                 </form>
                 <form id="_delete_logs" method="post">
                     <input type="hidden" name="_delete_logs" value="<?= md5(session_get('email')) ?>">
-                    <button id="submitBtn" type="submit"
-                        class="focus:outline-none text-white bg-red-400 hover:bg-red-500 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2 mr-2 mb-2 dark:focus:ring-red-900">Delete
+                    <button id="submitBtn" type="submit" class="focus:outline-none text-white bg-red-400 hover:bg-red-500 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2 mr-2 mb-2 dark:focus:ring-red-900">Delete
                         Logs</button>
                 </form>
             </div>
@@ -73,42 +63,33 @@ $all = implode(",", $logs);
                 </thead>
                 <tbody>
                     <?php $count = 0; ?>
-                    <?php foreach ($logs as $log): ?>
+                    <?php foreach ($logs as $log) : ?>
                         <?php $log = json_decode($log); ?>
-                        <tr @click="selected !== <?= $count; ?> ? selected = <?= $count; ?> : selected = null"
-                            class="bg-white hover:bg-gray-50 border-b dark:bg-gray-900 dark:border-gray-700">
+                        <tr @click="selected !== <?= $count; ?> ? selected = <?= $count; ?> : selected = null" class="bg-white hover:bg-gray-50 border-b dark:bg-gray-900 dark:border-gray-700">
                             <th scope="row" class="py-3 px-2 md:px-6 flex items-center">
-                                <?php if ($log->level === "Error"): ?>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                        stroke="currentColor" class="text-red-600 w-6 h-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
+                                <?php if ($log->level === "Error") : ?>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-red-600 w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
                                     </svg>
                                     <span class="pl-2 font-medium text-red-600 whitespace-nowrap dark:text-white">
                                         <?= $log->level; ?>
                                     </span>
-                                <?php elseif ($log->level === "Debug"): ?>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                        stroke="currentColor" class="text-blue-600 w-6 h-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
+                                <?php elseif ($log->level === "Debug") : ?>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-blue-600 w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
                                     </svg>
                                     <span class="pl-2 font-medium text-blue-600 whitespace-nowrap dark:text-white">
                                         <?= $log->level; ?>
                                     </span>
-                                <?php elseif ($log->level === "Warning"): ?>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                        stroke="currentColor" class="text-yellow-600 w-6 h-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M12 10.5v3.75m-9.303 3.376C1.83 19.126 2.914 21 4.645 21h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 4.88c-.866-1.501-3.032-1.501-3.898 0L2.697 17.626zM12 17.25h.007v.008H12v-.008z" />
+                                <?php elseif ($log->level === "Warning") : ?>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-yellow-600 w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 10.5v3.75m-9.303 3.376C1.83 19.126 2.914 21 4.645 21h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 4.88c-.866-1.501-3.032-1.501-3.898 0L2.697 17.626zM12 17.25h.007v.008H12v-.008z" />
                                     </svg><span class="pl-2 font-medium text-yellow-600 whitespace-nowrap dark:text-white">
                                         <?= $log->level; ?>
                                     </span>
-                                <?php else: ?>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                        stroke="currentColor" class="text-green-600 w-6 h-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+                                <?php else : ?>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-green-600 w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
                                     </svg>
 
                                     <span class="md:pl-2 font-medium text-green-600 whitespace-nowrap dark:text-white">
@@ -123,21 +104,29 @@ $all = implode(",", $logs);
                                 <?= ENV; ?>
                             </td>
                             <td class="py-3 px-2 md:px-6">
-                                <?= strstr($log->description, '<br />', true) !== false ? strstr($log->description, '<br />', true) : $log->description ?>
+                                <?php
+                                if (strstr($log->description, '<br />', true) !== false) {
+                                    echo strstr($log->description, '<br />', true);
+                                } elseif (strstr($log->description, '<br>', true) !== false) {
+                                    echo strstr($log->description, '<br>', true);
+                                } else {
+                                    echo $log->description;
+                                }
+                                ?>
                             </td>
 
                         </tr>
                         <tr x-show="selected == <?= $count; ?>">
 
                             <?php
-                                $classes = match ($log->level){
-                                    "Info" => "bg-green-50 border-b text-green-800 p-2",
-                                    "Debug" => "bg-blue-50 border-b text-blue-800 p-2",
-                                    "Error" => "bg-red-50 border-b text-red-800 p-2",
-                                    "Warning" => "bg-yellow-50 border-b text-yellow-800 p-2",
-                                }
-                           ?>
-                            <td class="<?=$classes?>" colspan="4">
+                            $classes = match ($log->level) {
+                                "Info" => "bg-green-50 border-b text-green-800 p-2",
+                                "Debug" => "bg-blue-50 border-b text-blue-800 p-2",
+                                "Error" => "bg-red-50 border-b text-red-800 p-2",
+                                "Warning" => "bg-yellow-50 border-b text-yellow-800 p-2",
+                            }
+                            ?>
+                            <td class="<?= $classes ?>" colspan="4">
                                 #
                                 <?= $log->id ?? $count; ?><br>
                                 <b>*Message*</b> <i>
@@ -172,12 +161,11 @@ $all = implode(",", $logs);
 
 <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 <script>
-
     const form = document.getElementById('_delete_logs')
     const submitBtn = document.getElementById('submitBtn');
     const url = '/:system:/logs/delete';
 
-    form.addEventListener('submit', function (event) {
+    form.addEventListener('submit', function(event) {
         event.preventDefault();
         submitBtn.classList.add('bg-blue-400', 'hover:bg-blue-500', 'focus:ring-blue-300');
         submitBtn.classList.remove('bg-red-400', 'hover:bg-red-500', 'focus:ring-red-300');
@@ -193,7 +181,7 @@ $all = implode(",", $logs);
             window.location.replace(window.location.pathname + window.location.search + window.location.hash);
 
 
-            setTimeout(function () {
+            setTimeout(function() {
                 submitBtn.classList.remove('bg-green-400', 'hover:bg-green-500', 'focus:ring-green-300');
                 submitBtn.classList.add('bg-red-400', 'hover:bg-red-500', 'focus:ring-red-300');
                 submitBtn.innerHTML = "Delete Logs";
@@ -202,10 +190,6 @@ $all = implode(",", $logs);
             submitBtn.innerHTML = "Could not Delete the logs!";
         });
     });
-
-
-
-
 </script>
 </body>
 
